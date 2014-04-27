fa366fae103c86221049757dadb440921641de2e
derikwhittaker <derik@graudo.com>
2014-04-27 05:59:57 -0500

    IoC Support

---
+++ src/SignalR.RealtimeLogger/App_Start/IoCBootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Web.Compilation;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using SignalR.RealtimeLogger.Domain.Services;
+
+namespace SignalR.RealTimeLogger
+{
+    public static class IoCBootstrapper
+    {
+        public static void Configure(HttpConfiguration httpConfiguration)
+        {
+            var builder = new ContainerBuilder();
+
+            var webAssembly = Assembly.GetExecutingAssembly();
+            var domainAssembly = Assembly.GetAssembly(typeof (ILogService));
+
+            builder.RegisterApiControllers(webAssembly);
+
+            builder.RegisterAssemblyTypes(webAssembly, domainAssembly)
+                .AsImplementedInterfaces();
+
+
+            var container = builder.Build();
+            var resolver = new AutofacWebApiDependencyResolver(container);
+
+            httpConfiguration.DependencyResolver = resolver;
+        }
+    }
+}
--- src/SignalR.RealtimeLogger/App_Start/WebApiConfig.cs
+++ src/SignalR.RealtimeLogger/App_Start/WebApiConfig.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Web.Http;
-using Autofac;
-using Autofac.Integration.WebApi;
-using SignalR.RealtimeLogger.Domain.Services;
 
 namespace SignalR.RealTimeLogger
 {
@@ -24,24 +20,6 @@ namespace SignalR.RealTimeLogger
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-        }
-    }
-
-    public static class IoCBootstrapper
-    {
-        public static void Configure(HttpConfiguration httpConfiguration)
-        {
-            var builder = new ContainerBuilder();
-
-            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
-
-            builder.RegisterType<LogService>().As<ILogService>().InstancePerApiRequest();
-
-            var container = builder.Build();
-
-            var resolver = new AutofacWebApiDependencyResolver(container);
-
-            httpConfiguration.DependencyResolver = resolver;
         }
     }
 }
